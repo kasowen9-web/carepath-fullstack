a7bded746ed4d1a8f23ba431618366d1d654c695
kasowen9-web <kasowen9@gmail.com>
2026-04-15 07:51:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -222,7 +222,7 @@ app.post("/api/tasks/complete", (req, res) => {
   res.json({ ok: true });
 });
 
-const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
