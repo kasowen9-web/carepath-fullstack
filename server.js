e35e8221ff169f4653c9df8e43785cc0fe5d83d9
kasowen9-web <kasowen9@gmail.com>
2026-04-15 07:34:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -227,5 +227,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
-  console.log(`Running on http://localhost:${PORT}`);
-});
+  
