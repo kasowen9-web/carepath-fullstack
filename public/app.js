f52226809e09a9e99d6c0516238e680f578a2014
kasowen9-web <kasowen9@gmail.com>
2026-04-14 19:10:38 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -17,8 +17,10 @@ async function postJSON(url, body) {
   const res = await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-
+    body: JSON.stringify(body)
+  });
   return res.json();
+}
 }
 
 function setView(view) {
